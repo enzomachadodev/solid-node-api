0925c1e6504be2f6e485feefa52fdf54d8f28689
m4chado <enzo653fm@gmail.com>
2023-12-17 18:57:58 -0300

    feat: should not be able to validate check-in with invalid check-in id

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { CheckIn } from "@prisma/client";
 import { ResourseNotFoundError } from "./errors/resource-not-found-error";
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string;
@@ -19,6 +21,14 @@ export class ValidateCheckInUseCase {
     const checkIn = await this.checkInsRepository.findById(checkInId);
 
     if (!checkIn) throw new ResourseNotFoundError();
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      "minutes",
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20)
+      throw new LateCheckInValidationError();
 
     checkIn.validated_at = new Date();
 
