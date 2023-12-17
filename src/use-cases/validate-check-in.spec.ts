bb594c4ae057477b33d0e2e37f14b066407130f2
m4chado <enzo653fm@gmail.com>
2023-12-17 18:58:13 -0300

    test: should not be able to validate check-in with invalid check-in id

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { ValidateCheckInUseCase } from "./validate-check-in";
 import { ResourseNotFoundError } from "./errors/resource-not-found-error";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -37,5 +38,24 @@ describe("Validate Check-In Use Case", () => {
         checkInId: "invalid-checkin-id",
       }),
     ).rejects.toBeInstanceOf(ResourseNotFoundError);
+  });
+
+  test("should not be able to validate check-in after 20 minutes of its creation", async () => {
+    jest.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const newCheckIn = await checkInsRepository.create({
+      gym_id: "gym_01",
+      user_id: "user_01",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    jest.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: newCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
