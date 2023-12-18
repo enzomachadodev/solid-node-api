89dd69f7bd227b3b0a81a3aad09a0b542d2bef82
m4chado <enzo653fm@gmail.com>
2023-12-18 11:11:36 -0300

    feat: update prisma users repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -4,21 +4,18 @@ import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
   async create(data: Prisma.UserCreateInput): Promise<User> {
-    const user = await prisma.user.create({ data });
-    return user;
+    return await prisma.user.create({ data });
   }
 
   async findByEmail(email: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
+    return await prisma.user.findUnique({
       where: { email },
     });
-    return user;
   }
 
   async findById(id: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
+    return await prisma.user.findUnique({
       where: { id },
     });
-    return user;
   }
 }
