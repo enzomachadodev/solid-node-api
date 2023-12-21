0cdacbda86b74b8abc039d6a8565836e2ae6084f
m4chado <enzo653fm@gmail.com>
2023-12-21 10:46:09 -0300

    test: create authenticate e2e test

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,1 +1,21 @@
-test("ok", () => {});
+import { app } from "@/app";
+import request from "supertest";
+
+describe("Authenticate (e2e)", () => {
+  const baseUrl: string = "/api/sessions";
+
+  test("should be able to authenticate", async () => {
+    await request(app).post("/api/users").send({
+      name: "New User",
+      email: "newuser@email.com",
+      password: "12345",
+    });
+
+    const response = await request(app).post(baseUrl).send({
+      email: "newuser@email.com",
+      password: "12345",
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
