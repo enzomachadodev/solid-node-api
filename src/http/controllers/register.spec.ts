b2c7a6c261243d555de7235d8ace0f30201ad7d4
m4chado <enzo653fm@gmail.com>
2023-12-21 10:43:13 -0300

    test: create register e2e test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,16 @@
-test("ok", () => {
-  console.log("AQUIIIIIIIIIIi@#@");
+import { app } from "@/app";
+import request from "supertest";
+
+describe("Register (e2e)", () => {
+  const baseUrl: string = "/api/users";
+
+  test("should be able to register", async () => {
+    const response = await request(app).post(baseUrl).send({
+      name: "New User",
+      email: "newuser@email.com",
+      password: "12345",
+    });
+
+    expect(response.status).toBe(201);
+  });
 });
