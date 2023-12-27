ada8e94a396f78df3c4d56bc3961720216b1ffa4
m4chado <enzo653fm@gmail.com>
2023-12-27 13:41:49 -0300

    fix: change database configurations for e2e tests

--- prisma/migrations/20231227143018_add_roles_to_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Roles" AS ENUM ('ADMIN', 'INSTRUCTOR', 'TRAINEE', 'MEMBER');
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "role" "Roles" NOT NULL DEFAULT 'MEMBER';
--- prisma/migrations/20231227163510_initial_migration/migration.sql
+++ prisma/migrations/20231227163510_initial_migration/migration.sql
@@ -1,9 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('ADMIN', 'INSTRUCTOR', 'TRAINEE', 'MEMBER');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password_hash" TEXT NOT NULL,
+    "role" "Roles" NOT NULL DEFAULT 'MEMBER',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -15,6 +19,8 @@ CREATE TABLE "check_ins" (
     "id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "validated_at" TIMESTAMP(3),
+    "user_id" TEXT NOT NULL,
+    "gym_id" TEXT NOT NULL,
 
     CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
 );
@@ -33,3 +39,9 @@ CREATE TABLE "gyms" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_gym_id_fkey" FOREIGN KEY ("gym_id") REFERENCES "gyms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
